9f317278707f392b2e8c72641879436829b24235
Formu1a <Toz1fq@bk.ru>
2022-04-12 03:06:31 +0300

    init:project

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./HomePage.css";
+
+class HomePage extends React.Component {
+    render() {
+        return <div className="homepage-container">ScadiWeb React </div>;
+    }
+}
+
+export default HomePage;
